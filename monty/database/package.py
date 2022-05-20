28eae0a0bd2c7adb04bf6d0ccc8f767dd3000c7c
onerandomusername <genericusername414+git@gmail.com>
2022-05-20 05:00:10 -0400

    fix: add back missing hidden field

--- monty/database/package.py
+++ monty/database/package.py
@@ -16,6 +16,7 @@ class PackageInfo(ormar.Model):
     name: str = ormar.String(primary_key=True, max_length=50, regex=r"^[a-z0-9_]+$")
     inventory_url: str = ormar.Text()
     base_url: Optional[str] = ormar.Text(nullable=True)
+    hidden: bool = ormar.Boolean(default=False, server_default="false", nullable=False)
     guilds_whitelist: Optional[List[int]] = ormar_pg_ext.ARRAY(
         item_type=sqlalchemy.BigInteger(),
         nullable=True,
